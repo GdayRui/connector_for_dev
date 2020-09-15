e2f96487d8e7d7f1b88993585d80ca424d163544
Rui <lirui.jobs@gmail.com>
2020-09-15 15:27:16 +1000

    create profile model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -16,6 +21,70 @@ router.get(
 			res.json(user);
 		} catch (err) {
 			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	});
+
+// @route 	POST api/auth
+// @desc 		Authenticate user & get token
+// @access 	Public
+router.post(
+	'/',
+	[
+		check('email', 'Please enter a valid email')
+			.isEmail(),
+		check('password', 'Password is required ')
+			.exists()
+	],
+	async (req, res) => {
+		console.log(req.body);
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+
+			// Check if user exists
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid Credential' }] });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid Credential' }] });
+			}
+
+			// Return JWT
+			const payload = {
+				user: {
+					id: user.id
+				}
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 }, // production set to 3600
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token }); // respons data is token
+				}
+			);
+
+			// res.send('User registered');
+
+		} catch (err) {
+			console.log(err.message);
 			res.status(500).send('Server error');
 		}
 	});
