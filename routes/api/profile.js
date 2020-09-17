95c0dd41f7aa5de9e59e275f45bc41dfdcb41fb3
Rui <lirui.jobs@gmail.com>
2020-09-17 14:35:06 +1000

    delete experience by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -188,7 +188,8 @@ router.delete(
 // @route 	PUT api/profile/experience
 // @desc 		Put profile experience
 // @access 	Private
-router.put('/experience',
+router.put(
+	'/experience',
 	[
 		auth,
 		[
@@ -242,5 +243,32 @@ router.put('/experience',
 		}
 
 	});
+
+// @route 	DELETE api/profile/experience/:exp_id
+// @desc 		Delete experience from profile
+// @access 	Private
+router.delete(
+	'/experience/:exp_id',
+	auth,
+	async (req, res) => {
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			// Get remove idx
+			const removeIndex = profile.experience
+				.map(item => item.id)
+				.indexOf(req.params.exp_id);
+
+			profile.experience.splice(removeIndex, 1);
+
+			await profile.save();
+			res.json(profile);
+
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+)
 
 module.exports = router;
